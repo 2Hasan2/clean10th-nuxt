91b33083ffee0d6c4313ea866000e2ce2c2b06c3
2Hasan2 <h01554176846@gmail.com>
2025-01-11 19:41:17 +0200

    2025-01-11 07:41:17 م

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,33 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: [// "@prisma/nuxt",
-  "@nuxt/ui", "@vueuse/nuxt", "nuxt-echarts", "@pinia/nuxt"],
+  modules: ["@nuxt/ui", "@vueuse/nuxt", "nuxt-echarts", "@pinia/nuxt", "@vite-pwa/nuxt"],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Nuxt App',
+      short_name: 'NuxtApp',
+      description: 'A Nuxt 3 application with PWA support',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    }
+  }
 });
